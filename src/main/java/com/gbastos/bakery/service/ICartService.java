8ae6f92d46fa320231bc5f8008b33612e67a09ff
Guilherme Borges Bastos <guilhermeborgesbastos@gmail.com>
2019-08-30 14:44:45 -0300

    Cart Service Interface
    - Created the Cart Service interface that will be used to guide the
    implementation and the Unit Tests.

---
+++ src/main/java/com/gbastos/bakery/service/ICartService.java
@@ -0,0 +1,38 @@
+package com.gbastos.bakery.service;
+
+import java.util.Map;
+import com.gbastos.bakery.model.Product;
+
+public interface ICartService {
+
+  /**
+   * Adds the product to the cart, in case it already exists in the cart, it increases its quantity,
+   * otherwise, it basically adds it to the cart.
+   *
+   * @param quantity product's quantity
+   * @param product
+   */
+  void addProduct(Integer quantity, Product product);
+
+  /**
+   * Retrieves the cart items copy.
+   *
+   * @return the map with the cart items.
+   */
+  Map<Product, Integer> retrieveCartItems();
+
+  /**
+   * Calculates the total value by summing the price of each item(Pack) into the cart multiplying
+   * its quantity by its price.
+   *
+   * @return the total cart's price.
+   */
+  Double calculateItemsTotal();
+
+  /**
+   * Checks if the cart it is empty.
+   *
+   * @return the true, if the cart is empty.
+   */
+  Boolean isEmpty();
+}
